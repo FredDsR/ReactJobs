bcaf484e709ab3b984242ac8a3cb775bec30e114
fred_reck <freddalsoglio@gmail.com>
2024-05-28 16:01:07 -0300

    full component site

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,18 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
 import JobListings from "./components/JobListings"
+import ViewAllJobs from "./components/ViewAllJobs"
 
 const App = () => {  
   return (
     <>
     <Navbar />
-    <Hero />
+    <Hero title="React Jobs" subtitle="The right path to your React Job"/>
     <HomeCards />
 
     <JobListings />
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <ViewAllJobs />
     </>
   )
 }
